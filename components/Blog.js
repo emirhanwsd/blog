b615734dc793c4e202afe49a72498a88b051687d
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-18 20:19:42 +0300

    designed responsive of Blog

--- components/Blog.js
+++ components/Blog.js
@@ -4,8 +4,8 @@ const Blog = props => {
     return (
         <div className="flex flex-col items-center">
             <h6 className="text-indigo-600 font-semibold uppercase mb-3">Introduction</h6>
-            <h3 className="text-3xl font-bold mb-6">{props.title}</h3>
-            <p className="text-xl text-gray-400 mb-4">{props.text}</p>
+            <h3 className="text-3xl font-bold mb-6 text-center">{props.title}</h3>
+            <p className="text-xl text-gray-400 mb-8 md:mb-4 text-center md:text-justify">{props.text}</p>
             <div className="w-full flex items-center justify-between pb-6">
                 <Link href="../">
                     <a className="px-6 py-2 text-gray-100 bg-indigo-600 hover:bg-indigo-700 rounded transition-colors">Go back home</a>
