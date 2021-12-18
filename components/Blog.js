b05d3dd09fb8a3903f0a6721cd29683735f7a188
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-18 19:05:47 +0300

    changed href value of links on Home

--- components/Blog.js
+++ components/Blog.js
@@ -6,7 +6,7 @@ const Blog = props => {
             <h6 className="text-gray-400 mb-2">{props.date}</h6>
             <h3 className="text-xl font-bold mb-4">{props.title}</h3>
             <p className="text-gray-400 mb-4 max-w-2xl">{props.text}</p>
-            <Link href={props.slug}>
+            <Link href={`/blog/${props.slug}`}>
                 <a className="text-indigo-500 hover:text-indigo-600 font-bold transition-colors">Read full article</a>
             </Link>
         </div>
