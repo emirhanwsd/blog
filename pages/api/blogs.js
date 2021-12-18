21db154f2ca2ee6fa716fb6ddb31cfef02362d2f
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-18 18:46:47 +0300

    added blogs api

---
+++ pages/api/blogs.js
@@ -0,0 +1,28 @@
+export default function request(request, result) {
+    result.status(200).json([
+        {
+            "title": "Boost your conversation rate",
+            "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cumque dicta error eum fugit id nam necessitatibus nesciunt quos repudiandae. Dolores laboriosam, quasi quod sed soluta tenetur unde veritatis voluptatum!",
+            "slug": "boost-your-conversation-rate",
+            "date": "Mar 16, 2021"
+        },
+        {
+            "title": "How to use search engine optimization to drive sales",
+            "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+            "slug": "how-to-use-search-engine-optimization-to-drive-sales",
+            "date": "Mar 10, 2021"
+        },
+        {
+            "title": "Improve your customer experience",
+            "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cumque dicta error eum fugit id nam necessitatibus nesciunt quos repudiandae.",
+            "slug": "improve-your-customer-experience",
+            "date": "Feb 12, 2021"
+        },
+        {
+            "title": "Writing effective landing page copy",
+            "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cumque dicta error eum fugit id nam necessitatibus nesciunt quos repudiandae. Dolores laboriosam.",
+            "slug": "writing-effective-landing-page-copy",
+            "date": "Jan 29, 2021"
+        }
+    ])
+}
