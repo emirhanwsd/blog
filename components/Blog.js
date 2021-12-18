0a5a6414555e9e907e9a5d2e875f1aeff7e620c0
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-18 20:00:47 +0300

    designed original Blog

--- components/Blog.js
+++ components/Blog.js
@@ -1,14 +1,18 @@
-import Link from "next/link";
+import Link from "next/link"
 
 const Blog = props => {
     return (
-        <div className="md:w-2/4 h-2/4">
-            <h6 className="text-gray-400 mb-2">{props.date}</h6>
-            <h3 className="text-xl font-bold mb-4">{props.title}</h3>
-            <p className="text-gray-400 mb-4 max-w-2xl">{props.text}</p>
-            <Link href={`/blog/${props.slug}`}>
-                <a className="text-indigo-500 hover:text-indigo-600 font-bold transition-colors">Read full article</a>
-            </Link>
+        <div className="flex flex-col items-center">
+            <h6 className="text-indigo-600 font-semibold uppercase mb-3">Introduction</h6>
+            <h3 className="text-3xl font-bold mb-6">{props.title}</h3>
+            <p className="text-xl text-gray-400 mb-4">{props.text}</p>
+            <div className="w-full flex items-center justify-between pb-6">
+                <Link href="../">
+                    <a className="px-6 py-2 text-gray-100 bg-indigo-600 hover:bg-indigo-700 rounded transition-colors">Go back home</a>
+                </Link>
+                <h6 className="text-gray-500">{props.date}</h6>
+            </div>
+
         </div>
     )
 }
