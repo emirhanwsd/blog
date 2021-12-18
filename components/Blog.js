c65cb3bbcc0d329fbed41937ade16197522326ef
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-18 17:23:13 +0300

    designed responsive of Blog component

--- components/Blog.js
+++ components/Blog.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Blog = props => {
     return (
-        <div className="w-2/4 h-2/4">
+        <div className="md:w-2/4 h-2/4">
             <h6 className="text-gray-400 mb-2">{props.date}</h6>
             <h3 className="text-xl font-bold mb-4">{props.title}</h3>
             <p className="text-gray-400 mb-4 max-w-2xl">{props.text}</p>
